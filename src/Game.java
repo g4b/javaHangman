37c87ae45018fc6d2ebcb6c075685ac1c910e6e9
gabe fantacone <gabrielfantacone@students.berkeley.net>
2019-02-12 14:25:49 -0800

    Keep working

--- src/Game.java
+++ src/Game.java
@@ -1,17 +1,31 @@
 import java.util.Random;
-import java.util.Scanner;
 
 public class Game {
 
-    public static void main(String[] args){
-        int guesses = 8;
+    int numGuesses;
+    int randomIndex;
+    String word;
+    String underscores;
+
+    public Game(){
+        this.numGuesses = 8;
         String[] words = {"bunny", "shirt", "music", "zoology", "textbook", "xylophone", "graph"};
-        int randomIndex = new Random().nextInt(words.length);
-        String word = words[randomIndex];
-        String underscores;
-        for(int i = 0; i < word.length(); i++){
-            // underscores += "_" --> not sure how to do this in Java?
+        this.randomIndex = new Random().nextInt(words.length);
+        this.word = words[randomIndex];
+        for (int i = 0; i < word.length(); i++){
+            this.underscores += "_";
         }
+    }
+
+    public void guessLetter(char letter){
+        char[] undArray = underscores.toCharArray();
+        for (int i = 0; i < word.length(); i++){
+            if (word.substring(i, i + 1).equals(letter)){
+                undArray[i] = letter;
+                this.numGuesses++;
+            }
+        }
+        this.numGuesses--;
     }
 
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
-import java.util.Random;
 
 public class Runner {
 
     public static void main(String[] args){
+
+        Game game = new Game();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Guess a letter");
+        System.out.println("Guess a letter:");
         String guess = sc.nextLine();
         if(sc.hasNextLine()){
             String input = sc.nextLine();
         } else {
             System.out.println("Sorry, that wasn't a string. Please try again.");
         }
+        game.guessLetter(sc.next(".").charAt(0));
+
     }
 }
