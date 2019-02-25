75058f532f5290f51006ce71767d36e7d0b4f902
gabe fantacone <gabrielfantacone@students.berkeley.net>
2019-02-25 14:39:26 -0800

    I guess one letter and the whole thing stops??

--- src/Game.java
+++ src/Game.java
@@ -6,43 +6,50 @@ public class Game {
     int numGuesses;
     int randomIndex;
     String word;
-    String underscores = "";
+    String[] undArray;
 
     public Game(){
         this.numGuesses = 8;
         String[] words = {"bunny", "shirt", "music", "zoology", "textbook", "xylophone", "graph"};
         this.randomIndex = new Random().nextInt(words.length);
         this.word = words[this.randomIndex];
+        this.undArray = new String[word.length()];
         for (int i = 0; i < word.length(); i++){
-            this.underscores += "_";
+            this.undArray[i] = "_";
         }
     }
 
-    public void setUnderscores(String underscores){
-        this.underscores = underscores;
+    public void setUnderscores(String[] undArray){
+        this.undArray = undArray;
     }
 
     public void guessLetter(String letter){
-        String[] undArray = new String[word.length()];
-        for (int i = 0; i < word.length(); i++){
-            undArray[i] = "_";
-        }
         for (int i = 0; i < word.length(); i++){
             if (word.substring(i, i + 1).equals(letter)){
                 undArray[i] = letter;
                 this.numGuesses++;
             }
         }
-        String newUnd = "";
+        setUnderscores(undArray);
+        this.toString("");
+        if (isCorrect(undArray)){
+            System.out.println("Congratulations! You solved the puzzle! Reload to play again.");
+            this.numGuesses = 0;
+        }
+    }
+
+    public void toString(String newUnd){
         for (int i = 0; i < undArray.length; i++){
             newUnd += undArray[i];
         }
-        this.showUnd();
-        this.numGuesses--;
+        System.out.println(newUnd);
     }
 
-    public void showUnd(){
-        System.out.println(this.underscores);
+    public boolean isCorrect(String[] undArray){
+        String[] wordArray = new String[word.length()];
+        for (int i = 0; i < word.length(); i++){
+            wordArray[i] = word.substring(i, i + 1);
+        }
+        return Arrays.equals(undArray, wordArray);
     }
-
 }
--- src/Runner.java
+++ src/Runner.java
@@ -5,13 +5,14 @@ public class Runner {
     public static void main(String[] args){
 
         Game game = new Game();
-        game.showUnd();
-        while (game.numGuesses <= 8){
+        game.toString("");
+        while (game.numGuesses < 9){
             Scanner sc = new Scanner(System.in);
             System.out.println("Guess a letter:");
             if(sc.hasNextLine()){
                 String guess = sc.nextLine();
                 game.guessLetter(guess);
+                game.numGuesses--;
             } else {
                 System.out.println("Sorry, that wasn't a string. Please try again.");
             }
