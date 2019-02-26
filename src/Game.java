ddc3e83281976f6eb6b5b154eb31126b743f9192
gabe fantacone <gabrielfantacone@students.berkeley.net>
2019-02-26 14:25:04 -0800

    Done

--- src/Game.java
+++ src/Game.java
@@ -7,16 +7,20 @@ public class Game {
     int randomIndex;
     String word;
     String[] undArray;
+    String[] guessedLetters;
+    int numOfGuess;
 
     public Game(){
         this.numGuesses = 8;
-        String[] words = {"bunny", "shirt", "music", "zoology", "textbook", "xylophone", "graph"};
+        String[] words = {"bunny", "shirt", "music", "prince", "knife", "graph", "parabola", "crumpet", "cypress", "zoology", "textbook", "xylophone", "epistemology", "onomatopoeia"};
         this.randomIndex = new Random().nextInt(words.length);
         this.word = words[this.randomIndex];
         this.undArray = new String[word.length()];
         for (int i = 0; i < word.length(); i++){
             this.undArray[i] = "_";
         }
+        this.guessedLetters = new String[30];
+        this.numOfGuess = 0;
     }
 
     public void setUnderscores(String[] undArray){
@@ -24,18 +28,38 @@ public class Game {
     }
 
     public void guessLetter(String letter){
-        for (int i = 0; i < word.length(); i++){
-            if (word.substring(i, i + 1).equals(letter)){
+        for (int i = 0; i < word.length(); i++) {
+            if (word.substring(i, i + 1).equals(letter)) {
                 undArray[i] = letter;
-                this.numGuesses++;
+                numGuesses++;
             }
         }
         setUnderscores(undArray);
         this.toString("");
-        if (isCorrect(undArray)){
-            System.out.println("Congratulations! You solved the puzzle! Reload to play again.");
+        if (isCorrect(undArray)) {
+            System.out.println("You Win!");
             this.numGuesses = 0;
         }
+        this.numOfGuess++;
+    }
+
+    public void checkGuessed(String letter){
+        if (this.numOfGuess == 0){
+            this.addGuessedLetter(letter);
+            this.guessLetter(letter);
+        } else {
+            if (guessed(letter)){
+                System.out.println("You have already guessed that letter. Please pick another one.");
+            } else {
+                this.addGuessedLetter(letter);
+                this.guessLetter(letter);
+            }
+        }
+    }
+
+    public void addGuessedLetter(String letter){
+        this.guessedLetters[this.numOfGuess] = letter;
+        System.out.println(this.guessedLetters);
     }
 
     public void toString(String newUnd){
@@ -51,5 +75,20 @@ public class Game {
             wordArray[i] = word.substring(i, i + 1);
         }
         return Arrays.equals(undArray, wordArray);
+    }
+
+    public boolean guessed(String letter){
+        for (int i = 0; i < numOfGuess; i++){
+            if (guessedLetters[i].equals(letter)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void lose(){
+        if (numGuesses == 0){
+            System.out.println("You Lose! The word was " + this.word);
+        }
     }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -6,17 +6,18 @@ public class Runner {
 
         Game game = new Game();
         game.toString("");
-        while (game.numGuesses < 9){
+        while (game.numGuesses > 0){
             Scanner sc = new Scanner(System.in);
             System.out.println("Guess a letter:");
             if(sc.hasNextLine()){
                 String guess = sc.nextLine();
-                game.guessLetter(guess);
+                game.checkGuessed(guess);
                 game.numGuesses--;
             } else {
                 System.out.println("Sorry, that wasn't a string. Please try again.");
             }
         }
+        game.lose();
     }
 }
 
